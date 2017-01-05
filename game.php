e1ff82e7986c52b24332b52fe34959f49b6df750
Danielius <danielius55@yahoo.com>
2017-01-05 12:28:05 +0200

    logic fixed
    added session, random number does'nt change

--- game.php
+++ game.php
@@ -1,16 +1,34 @@
 <?php
+	session_start();
+	
 	$message = "";
-	$number = 0;
+	$won = false;
 	
 	function generateRandom() {
-		$number = rand(1, 10);
+		$_SESSION['number'] = rand(1, 10);
 	}
 	
+	if(!isset($_SESSION['number']) ) generateRandom();
+	
+	if(isset($_POST['new_game'])){
+		generateRandom();
+	}
 	
 	if(isset($_POST['submit'])){
+		$number = $_SESSION['number'];
 		$guess=$_POST['guess'];
-		echo $guess;
+		if ($guess > $number) {
+			$message = "Skaičius yra mažesnis.";
+		} else if ($guess < $number) {
+			$message = "Skaičius yra didesnis.";
+		} else {
+			$won = true;
+			$message = "Atspėjot";
+		}
 	}
+	
+
+	
 
 ?>
 
@@ -18,19 +36,25 @@
 <html>
 <body>
 
-<h1>Atspėk skaičių</h1>
-<p> Skaičius yra intervale nuo 1 iki 10</p>
+	<h1>Atspėk skaičių</h1>
+	<p> Skaičius yra intervale nuo 1 iki 10</p>
 
-<form role="form" method="post" action="game.php">
-	<fieldset>
-		<label><span>Jūsų spėjimas: &nbsp;</span></label>
-		<input name="guess" type="number"  min="1" max="10" required autofocus></input>
-		
-		<input type="submit" value="Spėti" name="submit"></input>
-	</fieldset>
-</form>
+	<form role="form" method="post" action="game.php">
+		<fieldset>
+			<label><span>Jūsų spėjimas: &nbsp;</span></label>
+			<input name="guess" type="number"  min="1" max="10" required autofocus></input>
+			
+			<input type="submit" value="Spėti" name="submit"></input>
+		</fieldset>
+	</form>
 
-<p><?php echo $message ?></p>
-
+	<p><?php echo $message ?></p>
+	<?php if ($won) 
+		echo '
+		<form role="form" method="post" action="game.php">
+			<input type="submit" value="Iš naujo" name="new_game"></input>
+		<form>
+		';
+	?>
 </body>
 </html>
