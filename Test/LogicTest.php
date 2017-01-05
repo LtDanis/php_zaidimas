57776e64a2cfd50475816cdb8621a1187449ef64
Danielius <danielius55@yahoo.com>
2017-01-05 15:39:28 +0200

    unit tests working

--- Test/LogicTest.php
+++ Test/LogicTest.php
@@ -1,17 +1,27 @@
 <?php
+
+require 'Logic.php';
+
 class LogicTest extends \PHPUnit_Framework_TestCase
 {
+	
 	public function testRandomNumber()
 	{
-		$number = $this->generateRandom();
+		$logic = new Logic;
+		$number = $logic->generateRandom();
 		$this->assertLessThanOrEqual(10, $number);
 		$this->assertGreaterThanOrEqual(1, $number);
 	}
 	
 	public function testCompareNumbers()
 	{
+		$logic = new Logic;
 		$num1 = 3;
 		$num2 = 10;
 		$num3 = 5;
+		$this->assertEquals("Skaičius yra didesnis.", $logic->compareMessage($num1, $num2));
+		$this->assertEquals("Skaičius yra mažesnis.", $logic->compareMessage($num2, $num3));
+		$this->assertEquals("Atspėjot", $logic->compareMessage($num2, $num2));
 	}
 }
+?>
