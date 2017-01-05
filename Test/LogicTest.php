a2ccd1d5ba4735a3d3d2090f54b71ab895c31b57
Danielius <danielius55@yahoo.com>
2017-01-05 15:19:06 +0200

    assertEquals

--- Test/LogicTest.php
+++ Test/LogicTest.php
@@ -3,7 +3,7 @@ class LogicTest extends \PHPUnit_Framework_TestCase
 {
 	public function testRandomNumber()
 	{
-		$number = $generateRandom();
+		$number = $this->generateRandom();
 		$this->assertLessThanOrEqual(10, $number);
 		$this->assertGreaterThanOrEqual(1, $number);
 	}
@@ -13,8 +13,5 @@ class LogicTest extends \PHPUnit_Framework_TestCase
 		$num1 = 3;
 		$num2 = 10;
 		$num3 = 5;
-		$this->assert("Skaičius yra didesnis.", compareMessage($num1, $num2));
-		$this->assert("Skaičius yra mažesnis.", compareMessage($num2, $num3));
-		$this->assert("Atspėjot", compareMessage($num2, $num2));
 	}
 }
